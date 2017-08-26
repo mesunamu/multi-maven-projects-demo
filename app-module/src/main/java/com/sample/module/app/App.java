f181b4d0e824ea6e035119d43fb8250d144efecc
mesunamu <msn.murthy@intellectdesign.com>
2017-08-26 17:10:49 -0400

    Second commit

--- app-module/src/main/java/com/sample/module/app/App.java
+++ app-module/src/main/java/com/sample/module/app/App.java
@@ -4,6 +4,7 @@ import com.sample.module.util.DateUtil;
 
 public class App {
 
+	//this is to test
 	public static void main(String[] args) {
 		System.out.println("Hello World! Today is " + DateUtil.getToday());
 	}
