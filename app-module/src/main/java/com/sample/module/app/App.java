6d7658cc3a95bfe39b32977b08b31d7a777d74b8
mesunamu <msn.murthy@intellectdesign.com>
2017-08-26 17:03:49 -0400

    First Commit

---
+++ app-module/src/main/java/com/sample/module/app/App.java
@@ -0,0 +1,11 @@
+package com.sample.module.app;
+
+import com.sample.module.util.DateUtil;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World! Today is " + DateUtil.getToday());
+	}
+
+}
